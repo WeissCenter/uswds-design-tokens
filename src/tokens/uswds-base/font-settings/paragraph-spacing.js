9798b713946bc5aa54d68c198cbc88fd559774de
Marcelo Paiva <135261167+mpaiva-aem@users.noreply.github.com>
2023-10-20 16:01:46 +0000

    paragraph spacing primitives added

---
+++ src/tokens/uswds-base/font-settings/paragraph-spacing.js
@@ -0,0 +1,32 @@
+module.exports = {
+    "1": {
+        "value": "{font-settings.size.base} * 1",
+        "type": "paragraphSpacing",
+        "description": "Paragraph spacing 1"
+    },
+    "2": {
+        "value": "{font-settings.size.base} * 1.2",
+        "type": "paragraphSpacing",
+        "description": "Paragraph spacing 2"
+    },
+    "3": {
+        "value": "{font-settings.size.base} * 1.35",
+        "type": "paragraphSpacing",
+        "description": "Paragraph spacing 3"
+    },
+    "4": {
+        "value": "{font-settings.size.base} * 1.5",
+        "type": "paragraphSpacing",
+        "description": "Paragraph spacing 4"
+    },
+    "5": {
+        "value": "{font-settings.size.base} * 1.62",
+        "type": "paragraphSpacing",
+        "description": "Paragraph spacing 5"
+    },
+    "6": {
+        "value": "{font-settings.size.base} * 1.75",
+        "type": "paragraphSpacing",
+        "description": "Paragraph spacing 6"
+    }
+}
