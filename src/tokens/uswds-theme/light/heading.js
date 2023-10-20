7f594c8b96ef404812ce84e237b8c1a5fda6d250
Marcelo Paiva <135261167+mpaiva-aem@users.noreply.github.com>
2023-10-20 13:37:05 +0000

    testing heading compilation

--- src/tokens/uswds-theme/light/heading.js
+++ src/tokens/uswds-theme/light/heading.js
@@ -3,5 +3,20 @@ module.exports = {
         "value": "{spacing.205}",
         "type": "spacing",
         "description": "Margin top for headings"
+    },
+    "h1-temporary": {
+      "value": {
+        "fontFamily": "{font-settings.family.merriweather}"
+        // "fontWeight": "{fontWeights.source-sans-pro-0}",
+        // "lineHeight": "{lineHeights.0}",
+        // "fontSize": "{fontSize.0}",
+        // "letterSpacing": "{letterSpacing.0}",
+        // "paragraphSpacing": "{paragraphSpacing.0}",
+        // "paragraphIndent": "{paragraphIndent.0}",
+        // "textCase": "{textCase.none}",
+        // "textDecoration": "{textDecoration.none}"
+      },
+      "type": "typography"
     }
+  
 }
