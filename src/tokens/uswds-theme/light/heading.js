1415b1e63021337f4483961a8d039432741e8b67
Marcelo Paiva <135261167+mpaiva-aem@users.noreply.github.com>
2023-10-20 18:08:32 +0000

    prose text style

--- src/tokens/uswds-theme/light/heading.js
+++ src/tokens/uswds-theme/light/heading.js
@@ -17,5 +17,33 @@ module.exports = {
           // "textDecoration": "{textDecoration.none}"
         },
         "type": "typography"
+    },
+    "heading-1": {
+        "value": {
+          "fontFamily": "{font-settings.family.public}",
+          "fontWeight": "{font.weight-bold}",
+          "lineHeight": "{font.size-2xl} * {line-height.heading}",
+          "fontSize": "{font.size-2xl}",
+          "letterSpacing": "{font-settings.letter-spacing.1}",
+          "paragraphSpacing": "{font-settings.paragraph-spacing.auto}",
+          "textCase": "{font-settings.text-transform.normal}"
+          // "paragraphIndent": "{paragraphIndent.0}",
+          // "textDecoration": "{textDecoration.none}"
+        },
+        "type": "typography"
+    },
+    "prose": {
+        "value": {
+          "fontFamily": "{font.prose}",
+          "fontWeight": "{font.weight-bold}",
+          "lineHeight": "{font.size-prose} * {line-height.body}",
+          "fontSize": "{font.size-prose",
+          "letterSpacing": "{font-settings.letter-spacing.1}",
+          "paragraphSpacing": "{font-settings.paragraph-spacing.auto}",
+          "textCase": "{font-settings.text-transform.normal}"
+          // "paragraphIndent": "{paragraphIndent.0}",
+          // "textDecoration": "{textDecoration.none}"
+        },
+        "type": "typography"
     }
 }
