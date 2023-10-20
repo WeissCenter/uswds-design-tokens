d20bdb22049356fdf6dfaa79c275628c42a33132
Marcelo Paiva <135261167+mpaiva-aem@users.noreply.github.com>
2023-10-20 16:24:02 +0000

    text-transform values added

---
+++ src/tokens/uswds-base/font-settings/text-transform.js
@@ -0,0 +1,32 @@
+module.exports = {
+    "normal": {
+        "value": "none",
+        "type": "textCase",
+        "description": "Text case"
+    },
+    "uppercase": {
+        "value": "uppercase",
+        "type": "textCase",
+        "description": "Text case"
+    },
+    "lowercase": {
+        "value": "lowercase",
+        "type": "textCase",
+        "description": "Text case"
+    },
+    "capitalize": {
+        "value": "capitalize",
+        "type": "textCase",
+        "description": "Text case"
+    },
+    "smallCaps": {
+        "value": "small-caps",
+        "type": "textCase",
+        "description": "Text case"
+    },
+    "allSmallCaps": {
+        "value": "all-small-caps",
+        "type": "textCase",
+        "description": "Text case"
+    }
+}
